a1b557563ea2636a363180e8d975916921469bf9
Alexandre Dejous <alexandredejous@ma-3020-lx>
2019-05-13 14:55:58 +0200

    checkpt1

--- Projet.m
+++ Projet.m
@@ -2,4 +2,13 @@ clc;
 close all;
 clear all;
 
-[Num,Fe] = audioread('numero1.wav');
+[Num,Fe] = audioread('./pianoSoundFiles/ech1.wav');
+
+Te = 1/Fe;
+N=Fe;
+T=(0:Te:(length(Num)-1)*Te);
+
+figure (1)
+plot(T,Num(:,1))
+figure (2)
+plot(T,Num(:,2))
