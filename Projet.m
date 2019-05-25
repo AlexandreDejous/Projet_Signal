98d4a9a5a6f836518b02ae90970714c7cef090ee
Alexandre Dejous <alexandredejous@ma-3020-lx>
2019-05-25 04:22:37 +0200

    before oversampling

--- Projet.m
+++ Projet.m
@@ -3,7 +3,7 @@ close all;
 clear all;
 
 %import signal
-[Num,Fe] = audioread('./pianoSoundFiles/ech1.wav');
+[Num,Fe] = audioread('./pianoSoundFiles/piano.wav');
 
 %stereo to mono
 Num(:,1) = (Num(:,1) + Num(:,2)) / 2;
@@ -17,27 +17,29 @@ T=(0:Te:(length(Num)-1)*Te);
 %FNum = abs(fftshift(fft(Num)));
 
 %sliding window
-window = 50;
-nbWindow = floor(length(Num)/(2*window+1))%à changer
-for i = (1+window:2*window+1:length(Num)-window)
-	for j = (-window:window)
-		
-	end
+% window = 13486;
+% fullWindowSize = 2*window+1;
+% arrayWindow = zeros(fullWindowSize,1);%init array holding values of current window for fft
+% F = (  -Fe/2 : Fe/N : ((N/2)-1)*(Fe/N)  );%
+% nbWindow = floor(length(Num)/(fullWindowSize)); %number of entire windows we can put in Num 
+% for i = (1+window:fullWindowSize:nbWindow*fullWindowSize-window)
+% 	for j = (-window:window)
+% 		arrayWindow(window+j+1) = Num(i+j);
+% 		
+% 	end
+% 
+% end
+
+
+for i = (1:25)
+	figure (i);
+	[Num,Fe] = audioread(strcat('./pianoSoundFiles/ech',int2str(i),'.wav'));
+	spectrogram(Num(:,1),6000,0,6000,Fe,'yaxis');
 end
 
+%figure(2);
+%spectrogram(Num,6000,0,6000,44000,'yaxis')
 
 
-
-figure (1);
-plot(T,Num);
-
-figure(2);
-plot(F,FNum);
-
-figure(3);
-spectrogram(Num,256,250,256,Fe,'yaxis');
-
-
-window = 50
 
 %wt = cwt(Num);
