2b680ef9ba5147a90da104e4ce574d3328f318b3
Alexandre Dejous <alexandredejous@ma-3020-lx>
2019-05-25 05:02:25 +0200

    inetrpolation done

--- Projet.m
+++ Projet.m
@@ -4,10 +4,20 @@ clear all;
 
 %import signal
 [Num,Fe] = audioread('./pianoSoundFiles/piano.wav');
+surNum = zeros(length(2*length(Num)-1),1); %initialisation array d'acceuil du signal suréchantilloné de taille 2*Num-1
+for i =(1:length(Num(:,1)))
+	surNum(2*(i-1)+1,1) = Num(i,1); %attrib. des valeurs de num aux échantillons d'index 2k+1 de surNum
+	
+end
+for i =(1:length(Num(:,1))-1)
+	surNum(2*i,1) = 0.5*(Num(i,1)+Num(i+1,1));
+end
+
+
 
 %stereo to mono
-Num(:,1) = (Num(:,1) + Num(:,2)) / 2;
-Num(:,2) = [];
+% Num(:,1) = (Num(:,1) + Num(:,2)) / 2;
+% Num(:,2) = [];
 
 %classic variables init (T,Te,F,Fe... + FNum)
 Te = 1/Fe;
@@ -31,11 +41,11 @@ T=(0:Te:(length(Num)-1)*Te);
 % end
 
 
-for i = (1:25)
-	figure (i);
-	[Num,Fe] = audioread(strcat('./pianoSoundFiles/ech',int2str(i),'.wav'));
-	spectrogram(Num(:,1),6000,0,6000,Fe,'yaxis');
-end
+% for i = (1:25)
+% 	figure (i);
+% 	[Num,Fe] = audioread(strcat('./pianoSoundFiles/ech',int2str(i),'.wav'));
+% 	spectrogram(Num(:,1),6000,0,6000,Fe,'yaxis');
+% end
 
 %figure(2);
 %spectrogram(Num,6000,0,6000,44000,'yaxis')
